f00d326d5de470acaeb7e2e03ecad1369098c616
Kaio Garcia <kaiofbgarcia@gmail.com>
2023-03-06 16:34:46 -0300

    magic number att

--- Biblioteca/AlunoTest.java
+++ Biblioteca/AlunoTest.java
@@ -7,42 +7,54 @@ import java.util.List;
 public class AlunoTest {
     @Test
     public void testVerificaAlunoComCodigoValido() {
-        Aluno alunoComCodigo10 = new Aluno("10");
-        assertTrue(alunoComCodigo10.verificaAluno("10"));
+        // Verifica se o aluno é valido com os codigos validos 10 e 5
+        int codigoValido10 = 10;
+        Aluno alunoComCodigo10 = new Aluno(codigoValido10);
+        assertTrue(alunoComCodigo10.verificaAluno(codigoValido10));
 
-        Aluno alunoComCodigo5 = new Aluno("5");
-        assertTrue(alunoComCodigo5.verificaAluno("5"));
+        int codigoValido5 = 5;
+        Aluno alunoComCodigo5 = new Aluno(codigoValido5);
+        assertTrue(alunoComCodigo5.verificaAluno(codigoValido5));
     }
     @Test
     public void testVerificaAlunoComCodigoInvalido() {
-        Aluno alunoComCodigo20 = new Aluno("20");
-        assertFalse(alunoComCodigo20.verificaAluno("20"));
+        // Verifica se o aluno é valido com o codigo invalido 20
+        int codigoInvalido20 = 20;
+        Aluno alunoComCodigo20 = new Aluno(codigoInvalido20);
+        assertFalse(alunoComCodigo20.verificaAluno(codigoInvalido20));
     }
     @Test
     public void testVerificaDebitoSemDebito() {
-        Aluno alunoSemDebito = new Aluno("10");
+        int codigoValido10 = 10;
+        Aluno alunoSemDebito = new Aluno(codigoValido10);
         assertFalse(alunoSemDebito.verificaDebito());
     }
     @Test
     public void testVerificaDebitoComDebito() {
-        Aluno alunoComDebito = new Aluno("10");
+        int codigoValido10 = 10;
+        Aluno alunoComDebito = new Aluno(codigoValido10);
         Debito debito = new Debito(10);
         alunoComDebito.adicionarDebito(debito);
         assertTrue(alunoComDebito.verificaDebito());
     }
     @Test
     public void testEmprestarLivrosComListaVazia() {
-        Aluno alunoComCodigo10 = new Aluno("10");
+        int codigoValido10 = 10;
+        Aluno alunoComCodigo10 = new Aluno(codigoValido10);
         List<Livro> livrosVazia = new ArrayList<>();
         assertFalse(alunoComCodigo10.emprestarLivro(livrosVazia));
     }
     @Test
     public void testEmprestarLivrosComListaValida() {
-        Aluno alunoComCodigo10 = new Aluno("10");
+        int codigoValido10 = 10;
+        Aluno alunoComCodigo10 = new Aluno(codigoValido10);
 
-        Livro livro1 = new Livro("Livro1", 20);
-        Livro livro2 = new Livro("Livro2", 30);
-        Livro livro3 = new Livro("Livro3", 40);
+        int codigoLivro1 = 20;
+        int codigoLivro2 = 30;
+        int codigoLivro3 = 40;
+        Livro livro1 = new Livro("Livro1", codigoLivro1);
+        Livro livro2 = new Livro("Livro2", codigoLivro2);
+        Livro livro3 = new Livro("Livro3", codigoLivro3);
 
         List<Livro> livros = new ArrayList<>();
         livros.add(livro1);
--- Biblioteca/DebitoTest.java
+++ Biblioteca/DebitoTest.java
@@ -7,21 +7,24 @@ public class DebitoTest {
    @Test
    // Verifica se um débito com código 10 retorna true
    public void verificaDebitoComCodigo10() {
-      Debito debito = new Debito(10);
+      int codigoValido10 = 10;
+      Debito debito = new Debito(codigoValido10);
       boolean resultado = debito.verificaDebito();
       assertTrue(resultado, "Um débito com código 10 deveria retornar true");
    }
    @Test
    // Verifica se um débito com código 5 retorna false
    public void verificaDebitoComCodigo5() {
-      Debito debito = new Debito(5);
+      int codigoValido5 = 5;
+      Debito debito = new Debito(codigoValido5);
       boolean resultado = debito.verificaDebito();
       assertFalse(resultado, "Um débito com código 5 deveria retornar false");
    }
    @Test
    // Verifica se um débito com código diferente de 5 ou 10 retorna false
    public void verificaDebitoComOutroCodigo() {
-      Debito debito = new Debito(7);
+      int codigoInvalido = 7;
+      Debito debito = new Debito(codigoInvalido);
       boolean resultado = debito.verificaDebito();
       assertFalse(resultado, "Um débito com código diferente de 5 ou 10 deveria retornar false");
    }
